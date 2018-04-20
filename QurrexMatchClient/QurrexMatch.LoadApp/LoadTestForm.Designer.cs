74e03c80615248be543ce3312b883a13df73a340
AndreySt <andrey.sitaev@gmail.com>
2018-04-20 10:14:23 +0300

    Minor GUI update

--- QurrexMatchClient/QurrexMatch.LoadApp/LoadTestForm.Designer.cs
+++ QurrexMatchClient/QurrexMatch.LoadApp/LoadTestForm.Designer.cs
@@ -80,19 +80,17 @@
             this.panelLeft.Controls.Add(this.btnPreset01);
             this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
             this.panelLeft.Location = new System.Drawing.Point(0, 0);
-            this.panelLeft.Margin = new System.Windows.Forms.Padding(4);
             this.panelLeft.Name = "panelLeft";
-            this.panelLeft.Size = new System.Drawing.Size(233, 508);
+            this.panelLeft.Size = new System.Drawing.Size(175, 413);
             this.panelLeft.TabIndex = 0;
             this.panelLeft.Tag = "0";
             // 
             // lblConnectionStatus
             // 
             this.lblConnectionStatus.AutoSize = true;
-            this.lblConnectionStatus.Location = new System.Drawing.Point(78, 340);
-            this.lblConnectionStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblConnectionStatus.Location = new System.Drawing.Point(58, 276);
             this.lblConnectionStatus.Name = "lblConnectionStatus";
-            this.lblConnectionStatus.Size = new System.Drawing.Size(13, 17);
+            this.lblConnectionStatus.Size = new System.Drawing.Size(10, 13);
             this.lblConnectionStatus.TabIndex = 21;
             this.lblConnectionStatus.Text = "-";
             this.lblConnectionStatus.Visible = false;
@@ -100,20 +98,18 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 101);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(7, 82);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.Size = new System.Drawing.Size(103, 13);
             this.label2.TabIndex = 20;
             this.label2.Text = "payload modulations";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 11);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(7, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(118, 17);
+            this.label1.Size = new System.Drawing.Size(87, 13);
             this.label1.TabIndex = 19;
             this.label1.Text = "trading strategies";
             // 
@@ -121,10 +117,9 @@
             // 
             this.btnPreset08.ImageIndex = 0;
             this.btnPreset08.ImageList = this.imgListPresets;
-            this.btnPreset08.Location = new System.Drawing.Point(81, 180);
-            this.btnPreset08.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPreset08.Location = new System.Drawing.Point(8, 147);
             this.btnPreset08.Name = "btnPreset08";
-            this.btnPreset08.Size = new System.Drawing.Size(63, 52);
+            this.btnPreset08.Size = new System.Drawing.Size(47, 42);
             this.btnPreset08.TabIndex = 18;
             this.btnPreset08.Tag = "0";
             this.btnPreset08.UseVisualStyleBackColor = true;
@@ -148,10 +143,9 @@
             // 
             this.btnPreset07.ImageIndex = 0;
             this.btnPreset07.ImageList = this.imgListPresets;
-            this.btnPreset07.Location = new System.Drawing.Point(11, 180);
-            this.btnPreset07.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPreset07.Location = new System.Drawing.Point(113, 98);
             this.btnPreset07.Name = "btnPreset07";
-            this.btnPreset07.Size = new System.Drawing.Size(63, 52);
+            this.btnPreset07.Size = new System.Drawing.Size(47, 42);
             this.btnPreset07.TabIndex = 17;
             this.btnPreset07.Tag = "0";
             this.btnPreset07.UseVisualStyleBackColor = true;
@@ -160,10 +154,9 @@
             // 
             this.btnPreset06.ImageIndex = 0;
             this.btnPreset06.ImageList = this.imgListPresets;
-            this.btnPreset06.Location = new System.Drawing.Point(151, 121);
-            this.btnPreset06.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPreset06.Location = new System.Drawing.Point(61, 98);
             this.btnPreset06.Name = "btnPreset06";
-            this.btnPreset06.Size = new System.Drawing.Size(63, 52);
+            this.btnPreset06.Size = new System.Drawing.Size(47, 42);
             this.btnPreset06.TabIndex = 16;
             this.btnPreset06.Tag = "0";
             this.btnPreset06.UseVisualStyleBackColor = true;
@@ -172,10 +165,9 @@
             // 
             this.btnPreset05.ImageIndex = 0;
             this.btnPreset05.ImageList = this.imgListPresets;
-            this.btnPreset05.Location = new System.Drawing.Point(81, 121);
-            this.btnPreset05.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPreset05.Location = new System.Drawing.Point(8, 98);
             this.btnPreset05.Name = "btnPreset05";
-            this.btnPreset05.Size = new System.Drawing.Size(63, 52);
+            this.btnPreset05.Size = new System.Drawing.Size(47, 42);
             this.btnPreset05.TabIndex = 15;
             this.btnPreset05.Tag = "0";
             this.btnPreset05.UseVisualStyleBackColor = true;
@@ -184,10 +176,9 @@
             // 
             this.btnPreset04.ImageIndex = 0;
             this.btnPreset04.ImageList = this.imgListPresets;
-            this.btnPreset04.Location = new System.Drawing.Point(11, 121);
-            this.btnPreset04.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPreset04.Location = new System.Drawing.Point(113, 25);
             this.btnPreset04.Name = "btnPreset04";
-            this.btnPreset04.Size = new System.Drawing.Size(63, 52);
+            this.btnPreset04.Size = new System.Drawing.Size(47, 42);
             this.btnPreset04.TabIndex = 14;
             this.btnPreset04.Tag = "0";
             this.btnPreset04.UseVisualStyleBackColor = true;
@@ -196,10 +187,9 @@
             // 
             this.btnPreset03.ImageIndex = 0;
             this.btnPreset03.ImageList = this.imgListPresets;
-            this.btnPreset03.Location = new System.Drawing.Point(151, 181);
-            this.btnPreset03.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPreset03.Location = new System.Drawing.Point(61, 147);
             this.btnPreset03.Name = "btnPreset03";
-            this.btnPreset03.Size = new System.Drawing.Size(63, 52);
+            this.btnPreset03.Size = new System.Drawing.Size(47, 42);
             this.btnPreset03.TabIndex = 13;
             this.btnPreset03.Tag = "0";
             this.btnPreset03.UseVisualStyleBackColor = true;
@@ -209,10 +199,9 @@
             this.btnStop.Enabled = false;
             this.btnStop.ImageIndex = 1;
             this.btnStop.ImageList = this.imgListPresets;
-            this.btnStop.Location = new System.Drawing.Point(11, 322);
-            this.btnStop.Margin = new System.Windows.Forms.Padding(4);
+            this.btnStop.Location = new System.Drawing.Point(8, 262);
             this.btnStop.Name = "btnStop";
-            this.btnStop.Size = new System.Drawing.Size(63, 52);
+            this.btnStop.Size = new System.Drawing.Size(47, 42);
             this.btnStop.TabIndex = 12;
             this.btnStop.Tag = "0";
             this.btnStop.UseVisualStyleBackColor = true;
@@ -222,10 +211,9 @@
             // 
             this.btnManTest.ImageIndex = 0;
             this.btnManTest.ImageList = this.imgListPresets;
-            this.btnManTest.Location = new System.Drawing.Point(11, 262);
-            this.btnManTest.Margin = new System.Windows.Forms.Padding(4);
+            this.btnManTest.Location = new System.Drawing.Point(8, 213);
             this.btnManTest.Name = "btnManTest";
-            this.btnManTest.Size = new System.Drawing.Size(63, 52);
+            this.btnManTest.Size = new System.Drawing.Size(47, 42);
             this.btnManTest.TabIndex = 11;
             this.btnManTest.Tag = "0";
             this.btnManTest.UseVisualStyleBackColor = true;
@@ -235,21 +223,18 @@
             // 
             this.groupBox1.Controls.Add(this.btnCommonSets);
             this.groupBox1.Controls.Add(this.btnSetupConnection);
-            this.groupBox1.Location = new System.Drawing.Point(11, 382);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Location = new System.Drawing.Point(8, 310);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(212, 102);
+            this.groupBox1.Size = new System.Drawing.Size(159, 83);
             this.groupBox1.TabIndex = 10;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "settings";
             // 
             // btnCommonSets
             // 
-            this.btnCommonSets.Location = new System.Drawing.Point(5, 31);
-            this.btnCommonSets.Margin = new System.Windows.Forms.Padding(4);
+            this.btnCommonSets.Location = new System.Drawing.Point(4, 25);
             this.btnCommonSets.Name = "btnCommonSets";
-            this.btnCommonSets.Size = new System.Drawing.Size(199, 28);
+            this.btnCommonSets.Size = new System.Drawing.Size(149, 23);
             this.btnCommonSets.TabIndex = 7;
             this.btnCommonSets.Text = "Common Settings ...";
             this.btnCommonSets.UseVisualStyleBackColor = true;
@@ -257,10 +242,9 @@
             // 
             // btnSetupConnection
             // 
-            this.btnSetupConnection.Location = new System.Drawing.Point(5, 66);
-            this.btnSetupConnection.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSetupConnection.Location = new System.Drawing.Point(4, 54);
             this.btnSetupConnection.Name = "btnSetupConnection";
-            this.btnSetupConnection.Size = new System.Drawing.Size(199, 28);
+            this.btnSetupConnection.Size = new System.Drawing.Size(149, 23);
             this.btnSetupConnection.TabIndex = 6;
             this.btnSetupConnection.Text = "Connection Settings ...";
             this.btnSetupConnection.UseVisualStyleBackColor = true;
@@ -270,10 +254,9 @@
             // 
             this.btnPreset02.ImageIndex = 0;
             this.btnPreset02.ImageList = this.imgListPresets;
-            this.btnPreset02.Location = new System.Drawing.Point(81, 31);
-            this.btnPreset02.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPreset02.Location = new System.Drawing.Point(61, 25);
             this.btnPreset02.Name = "btnPreset02";
-            this.btnPreset02.Size = new System.Drawing.Size(63, 52);
+            this.btnPreset02.Size = new System.Drawing.Size(47, 42);
             this.btnPreset02.TabIndex = 8;
             this.btnPreset02.Tag = "0";
             this.btnPreset02.UseVisualStyleBackColor = true;
@@ -282,10 +265,9 @@
             // 
             this.btnPreset01.ImageIndex = 0;
             this.btnPreset01.ImageList = this.imgListPresets;
-            this.btnPreset01.Location = new System.Drawing.Point(11, 31);
-            this.btnPreset01.Margin = new System.Windows.Forms.Padding(4);
+            this.btnPreset01.Location = new System.Drawing.Point(8, 25);
             this.btnPreset01.Name = "btnPreset01";
-            this.btnPreset01.Size = new System.Drawing.Size(63, 52);
+            this.btnPreset01.Size = new System.Drawing.Size(47, 42);
             this.btnPreset01.TabIndex = 7;
             this.btnPreset01.Tag = "0";
             this.btnPreset01.UseVisualStyleBackColor = true;
@@ -295,22 +277,20 @@
             this.tabControlMain.Controls.Add(this.tabPageLog);
             this.tabControlMain.Controls.Add(this.tabPageReport);
             this.tabControlMain.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControlMain.Location = new System.Drawing.Point(233, 0);
-            this.tabControlMain.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControlMain.Location = new System.Drawing.Point(175, 0);
             this.tabControlMain.Name = "tabControlMain";
             this.tabControlMain.SelectedIndex = 0;
-            this.tabControlMain.Size = new System.Drawing.Size(828, 508);
+            this.tabControlMain.Size = new System.Drawing.Size(621, 413);
             this.tabControlMain.TabIndex = 1;
             this.tabControlMain.SelectedIndexChanged += new System.EventHandler(this.tabControlMain_SelectedIndexChanged);
             // 
             // tabPageLog
             // 
             this.tabPageLog.Controls.Add(this.tbLog);
-            this.tabPageLog.Location = new System.Drawing.Point(4, 25);
-            this.tabPageLog.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPageLog.Location = new System.Drawing.Point(4, 22);
             this.tabPageLog.Name = "tabPageLog";
-            this.tabPageLog.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPageLog.Size = new System.Drawing.Size(820, 479);
+            this.tabPageLog.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPageLog.Size = new System.Drawing.Size(613, 387);
             this.tabPageLog.TabIndex = 0;
             this.tabPageLog.Text = "Log";
             this.tabPageLog.UseVisualStyleBackColor = true;
@@ -321,10 +301,9 @@
             this.tbLog.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tbLog.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.tbLog.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
-            this.tbLog.Location = new System.Drawing.Point(4, 4);
-            this.tbLog.Margin = new System.Windows.Forms.Padding(4);
+            this.tbLog.Location = new System.Drawing.Point(3, 3);
             this.tbLog.Name = "tbLog";
-            this.tbLog.Size = new System.Drawing.Size(812, 471);
+            this.tbLog.Size = new System.Drawing.Size(607, 381);
             this.tbLog.TabIndex = 0;
             this.tbLog.Text = "";
             // 
@@ -332,11 +311,10 @@
             // 
             this.tabPageReport.Controls.Add(this.cbReports);
             this.tabPageReport.Controls.Add(this.btnOpenReport);
-            this.tabPageReport.Location = new System.Drawing.Point(4, 25);
-            this.tabPageReport.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPageReport.Location = new System.Drawing.Point(4, 22);
             this.tabPageReport.Name = "tabPageReport";
-            this.tabPageReport.Padding = new System.Windows.Forms.Padding(4);
-            this.tabPageReport.Size = new System.Drawing.Size(820, 479);
+            this.tabPageReport.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this.tabPageReport.Size = new System.Drawing.Size(613, 387);
             this.tabPageReport.TabIndex = 1;
             this.tabPageReport.Text = "Report";
             this.tabPageReport.UseVisualStyleBackColor = true;
@@ -345,18 +323,16 @@
             // 
             this.cbReports.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbReports.FormattingEnabled = true;
-            this.cbReports.Location = new System.Drawing.Point(8, 48);
-            this.cbReports.Margin = new System.Windows.Forms.Padding(4);
+            this.cbReports.Location = new System.Drawing.Point(6, 39);
             this.cbReports.Name = "cbReports";
-            this.cbReports.Size = new System.Drawing.Size(241, 24);
+            this.cbReports.Size = new System.Drawing.Size(182, 21);
             this.cbReports.TabIndex = 1;
             // 
             // btnOpenReport
             // 
-            this.btnOpenReport.Location = new System.Drawing.Point(8, 12);
-            this.btnOpenReport.Margin = new System.Windows.Forms.Padding(4);
+            this.btnOpenReport.Location = new System.Drawing.Point(6, 10);
             this.btnOpenReport.Name = "btnOpenReport";
-            this.btnOpenReport.Size = new System.Drawing.Size(243, 28);
+            this.btnOpenReport.Size = new System.Drawing.Size(182, 23);
             this.btnOpenReport.TabIndex = 0;
             this.btnOpenReport.Text = "Open Report";
             this.btnOpenReport.UseVisualStyleBackColor = true;
@@ -364,12 +340,11 @@
             // 
             // LoadTestForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1061, 508);
+            this.ClientSize = new System.Drawing.Size(796, 413);
             this.Controls.Add(this.tabControlMain);
             this.Controls.Add(this.panelLeft);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "LoadTestForm";
             this.Text = "Qurrex Match Load Test";
             this.panelLeft.ResumeLayout(false);
